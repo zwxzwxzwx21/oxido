76039e5ee86fa96168b74384c2154fa02dbc40c5
zwxzwxzwx21 <111466345+zwxzwxzwx21@users.noreply.github.com>
2024-11-13 16:03:58 +0100

    Jeśli wszystko dobrze zrozumiałem, to powinno być wszystko

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# oxido
-this is my optional description, witam!
+### Instrukcja uruchomienia
+
+1. *Wystarczy otworzyć kod w interpreterze i uruchomić go*.
+- Można również otworzyć go za pomocą terminala wpisując "cmd" w lokalizacji plików a następnie wpisując "python oxido.py".
+- W przypadku braku bibliotek należy zainstalować "requests" i "bs4" nie umieszczam ich w osobnym pliku gdyż zostałem o to poproszony.
+
+#### Sposób działania
+
+1. Na początku skrypt łączy się z OpenAI poprzez mój klucz API.
+2. Utworzona jest flaga, która sprawdza do którego pliku powinien być wpisany wynik naszych promptów.
+3. Tworzony jest placeholder, który będzie przetrzymywać wynik jednego z promptów, gdyż jest on potrzebny później, a nie chcemy by został on nadpisany.
+4. Następnie iterujemy przez każdy prompt w pętli, pierwszy wytwarza nam treść przekształconego artykułu, po czym zmienia flage, by z następną iteracją, ta wpisała wartość drugiego prompta w inne pliki.
+5. Po wyjściu z pętli mamy już utworzone nasze 3 pliki, ale podglad.html jest wybrakowany, ponieważ nie ma jeszcze wpisanego artykułu, robimy więc to po pętli:
+- Otwieramy plik i szukamy części <body> w kodzie HTML
+- Czyścimy wszystko co jest w części body (kod generowany jest przez AI, więc jest szansa, że treść polecenia w którym zakazane jest wpisywanie jakiegokolwiek kodu, mogłabyć po prostu ominięta, urok pracy z AI :) )
+- Dopisujemy do części <body> nasz artykuł i wpisujemy do w podglad.html
+
+#### Uwagi
+
+GitHub blokuje klucze jeśli te zostaną załączone z kodem, więc postanowiłem swojego klucza API nie dołączać, by wszystko dobrze działało, wystarczy w variable "key" wpisać swój własny klucz (linia 8)
--- oxido.py
+++ oxido.py
@@ -1,34 +1,68 @@
 import requests
 import os 
-# it does conenct and works just fine
+
 url_test = "https://api.openai.com/v1/chat/completions"
 # otworzenie pliku w folderze, pozniej trzeba bedzie var tekst dac do content by ai go przerobilo
 with open("resc artykulu.txt",'r') as tresc:
     tekst = tresc.read()
-key =  "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"
+key =  "Wpisz swój klucz" ### 
 headers = \
 {
     "Authorization": f"Bearer {key}",
     "Content-Type": "application/json"
 }
-content = f"""przekształć poniższy artykuł w woniższy sposób: \n \
-            - użyj tagów html jak h1, p, by tekst był odpowiednio rozmieszczony na stronie \n \
-            - określ pola w których możnaby usytuować grafiki, użyj img z atrybutem src = "image_placeholder.jpg" \n \
-            - do każdego pola graficznego, dodaj artybut alt wraz z dokładnym promptem, który można użyć do wygenerowania grafiki, umieść również podpisy używając odpowieniego taki html \n \
-            - nie uzywaj CSS ani JavaScript, wszystko musi zawierać się wyłącznie między <body> \n \
-            oto artykuł: \n {tekst}"""
-data = \
-{
-    "model": "gpt-3.5-turbo",
-    "messages": [{"role": "user", "content": content}]
-}
-#need to change json to string so it prints out well
-response = requests.post(url_test,headers=headers,json=data)
 
-# in the response we look for first response and we take the content of the reply, so it looks readable!
+content =   [
+                f"""przekształć poniższy artykuł w opisany sposób: \n \
+                - użyj tagów html jak h1, p, by tekst był odpowiednio rozmieszczony na stronie \n \
+                - określ pola w których możnaby usytuować grafiki, użyj img z atrybutem src = "image_placeholder.jpg" \n \
+                - do każdego pola graficznego, dodaj artybut alt wraz z dokładnym promptem, który można użyć do wygenerowania grafiki, umieść również podpisy używając odpowieniego taki html \n \
+                - nie uzywaj CSS ani JavaScript, wszystko musi zawierać się wyłącznie między <body> \n \
+                oto artykuł: \n {tekst}""",
+                f"""wygeneruj kod HTML, który zawiera style CSS i skrypt JavaScript, umożliwiające wyświetlanie oraz wizualizację tekstu w przeglądarce. Sekcja <body> powinna być całkowicie pusta (czyli nie zawierać żadnych elementów ani tekstu), a kod CSS i JavaScript muszą być wbudowane bezpośrednio w kod HTML, ale bez treści w <body>, ani nie powinny wyświetlać żadnego tekstu na stronie."""
+            
+            ]
+# robimy petle by robic 2 prompty, lepsza czytelnosc
 
+prompt_number = 1
+# zapisujemy artykul w stringu zeby pozniej moc go latwo wkleic do pliku podglad.html
+artykul = ''
+for prompt in content:
+    
+    data = \
+    {
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": prompt}]
+    }
+    #zmiana na string 
+    response = requests.post(url_test,headers=headers,json=data)
 
-example_text = response.json()['choices'][0]['message']['content']
+    # interesuje nas tylko content string,
 
-with open("artykul.html",'w') as plik:
-    plik.write(example_text)
+    example_text = response.json()['choices'][0]['message']['content']
+
+    if prompt_number == 1:
+        with open("artykul.html",'w') as plik:
+            plik.write(example_text)
+            artykul = example_text
+    else:
+        with open("szablon.html",'w') as plik:
+            plik.write(example_text)
+        with open("podglad.html",'w') as plik_2:
+            plik_2.write(example_text)
+    prompt_number += 1 
+
+# zmieniamy plik podglad.html gdyz jeszcze musimy do niego wkleic nasz artykul
+from bs4 import BeautifulSoup
+with open("podglad.html",'r',encoding='utf-8') as plik:
+    parser = BeautifulSoup(plik,"html.parser")
+
+body_part = parser.find("body")
+if body_part:
+    # nie jest potrzebne ale cuda sie zdarzają
+    body_part.clear()
+
+    body_part.append(BeautifulSoup(artykul,'html.parser'))
+
+with open('podglad.html', "w", encoding="utf-8") as plik:
+    plik.write(str(parser))
