6250428fd5810af680d60a3316334c6cbefdc9ac
zwxzwxzwx21 <111466345+zwxzwxzwx21@users.noreply.github.com>
2024-11-13 14:22:17 +0100

    i hope this time my api key will not be disabled because i want to test it for a second :)) (everything works, at least to my understanding of what should be done, pushing because that is some progress anyway)

--- oxido.py
+++ oxido.py
@@ -5,7 +5,7 @@ url_test = "https://api.openai.com/v1/chat/completions"
 # otworzenie pliku w folderze, pozniej trzeba bedzie var tekst dac do content by ai go przerobilo
 with open("resc artykulu.txt",'r') as tresc:
     tekst = tresc.read()
-key =  "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"
+key =  "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"
 headers = \
 {
     "Authorization": f"Bearer {key}",
@@ -27,19 +27,8 @@ response = requests.post(url_test,headers=headers,json=data)
 
 # in the response we look for first response and we take the content of the reply, so it looks readable!
 
-#print(response.json()['choices'][0]['message']['content']) 
-# this one should check for choices in content,
-if response.status_code == 200:
-    response_text = response.json()
-    if 'choices' in response_text and len(response_text['choices'] > 0):
 
+example_text = response.json()['choices'][0]['message']['content']
 
-        #print(tekst)
-        # i dont really want to use my tokens that often so its a testcase first too see if writing to html file works just fine
-        example_text = response.json()['choices'][0]['message']['content']
-
-        with open("artykul.html",'w') as plik:
-            plik.write(example_text)
-    else:
-        print('there is no choices somehow')
-else: print('something went wrong')
+with open("artykul.html",'w') as plik:
+    plik.write(example_text)
